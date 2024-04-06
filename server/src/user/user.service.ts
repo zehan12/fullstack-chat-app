8284b5605394426b3a20c9366013a1c156f016f8
Zehan Khan <zkhan@hootboard.com>
2024-04-07 01:27:07 +0530

    feat: :sparkles: add create user service

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateUserDto) {
+    const userExits = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (userExits) throw new ConflictException('email duplicate');
+    const newUser = await this.prisma.user.create({
+      data: { ...dto, password: await hash(dto.password, 10) },
+    });
+    newUser.password = '**********';
+    return newUser;
+  }
+}
