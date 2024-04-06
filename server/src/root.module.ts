cf8ca5236757819817877ca0638e13b177cb3000
Zehan Khan <zkhan@hootboard.com>
2024-04-07 00:30:56 +0530

    feat: :technologist: create user resources

--- server/src/root.module.ts
+++ server/src/root.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, UsersModule],
   controllers: [],
   providers: [PrismaService],
 })
