419d2c7c0a0bd765fa069f4b99829387cd24a4d9
Zehan Khan <zkhan@hootboard.com>
2024-04-07 00:36:50 +0530

    feat: :crown: add create user dto

--- server/src/root.module.ts
+++ server/src/root.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UserModule, UsersModule],
+  imports: [UserModule],
   controllers: [],
   providers: [PrismaService],
 })
