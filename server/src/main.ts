a668132d834e15bdb727d6eb92392f376657a3df
Zehan Khan <zkhan@hootboard.com>
2024-04-07 00:19:31 +0530

    feat: :card_file_box: install and init prisma

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,6 @@ import { RootModule } from './root.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
