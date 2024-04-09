5475697adb2b8f64e45ed64343b1c4d822fdff43
Zehan Khan <zkhan@hootboard.com>
2024-04-09 12:56:06 +0530

    enable cors in nest app

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -3,6 +3,7 @@ import { RootModule } from './root.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
+  app.enableCors();
   await app.listen(4000);
 }
 bootstrap();
